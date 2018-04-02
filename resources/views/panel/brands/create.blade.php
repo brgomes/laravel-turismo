0892c1a87e934c3a5eb4cdf771affaad16050ddc
Bruno Roberto Gomes <bruno@unirg.edu.br>
2018-04-02 10:23:52 -0300

    3.6 - Validando o cadastro

--- resources/views/panel/brands/create.blade.php
+++ resources/views/panel/brands/create.blade.php
@@ -17,6 +17,16 @@
 </div>
 
 <div class="content-din">
+	@if(isset($errors) && $errors->any())
+		<div class="alert alert-warning">
+			<ul>
+				@foreach ($errors->all() as $error)
+					<li>{{$error}}</li>
+				@endforeach
+			</ul>
+		</div>
+	@endif
+
 	<form class="form form-search form-ds" action="{{route('brands.store')}}" method="post">
 		<div class="form-group">
 			<input type="text" name="name" placeholder="Nome" class="form-control">
