f7b314c265694d04ef053492cc8fffb997ea6511
Bruno Roberto Gomes <bruno@unirg.edu.br>
2018-04-02 15:10:50 -0300

    4.1 - Listagem de aviões

---
+++ resources/views/panel/planes/index.blade.php
@@ -0,0 +1,77 @@
+@extends('panel.layouts.app')
+
+@section('content')
+
+<div class="bred">
+    <a href="{{route('panel')}}" class="bred">Home  ></a>
+    <a href="{{route('planes.index')}}" class="bred">Aviões</a>
+</div>
+
+<div class="title-pg">
+	<h1 class="title-pg">Aviões</h1>
+</div>
+
+<div class="content-din bg-white">
+	<div class="form-search">
+		{!! Form::open(['route' => 'brands.search', 'class' => 'form form-inline']) !!}
+			{!! Form::text('key_search', null, ['class' => 'form-control', 'placeholder' => 'Nome']) !!}
+
+			<button class="btn btn-search">Pesquisar</button>
+		{!! Form::close() !!}
+
+		@if(isset($dataForm))
+			<div class="alert alert-info">
+				<a href="{{route('planes.index')}}"><i class="fa fa-refresh" aria-hidden="true"></i></a>
+				Resultados para: <strong>{{$dataForm['key_search']}}</strong>
+			</div>
+		@endif
+	</div>
+
+	<div class="messages">
+		@include('panel.includes.alerts')
+	</div>
+
+	<div class="class-btn-insert">
+		<a href="{{route('planes.create')}}" class="btn-insert">
+			<span class="glyphicon glyphicon-plus"></span>
+			Cadastrar avião
+		</a>
+	</div>
+
+	<table class="table table-striped">
+		<tr>
+			<th>Nome</th>
+			<th>Marca</th>
+			<th>Total de passageiros</th>
+			<th width="150">Ações</th>
+		</tr>
+
+		@forelse($planes as $plane)
+			<tr>
+				<td>{{$plane->name}}</td>
+				<td>...</td>
+				<td>{{$plane->qty_passangers}}</td>
+				<td>
+					<a href="{{route('planes.edit', $plane->id)}}" class="edit">Edit</a>
+					<a href="{{route('planes.show', $plane->id)}}" class="delete">View</a>
+				</td>
+			</tr>
+		@empty
+			<tr>
+				<td colspan="200">Nenhum item cadastrado</td>
+			</tr>
+		@endforelse
+	</table>
+
+	<p>
+		Total encontrado: <strong>{{$planes->total()}}</strong>
+	</p>
+
+	@if(isset($dataForm))
+		{!! $planes->appends($dataForm)->links() !!}
+	@else
+		{!! $planes->links() !!}
+	@endif
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
 $this->group(['prefix' => 'panel', 'namespace' => 'Panel'], function() {
+
 	$this->any('brands/search', 'BrandController@search')->name('brands.search');
+	$this->resource('brands', 'BrandController');
+
+	$this->resource('planes', 'PlaneController');
 
 	$this->get('/', 'PanelController@index')->name('panel');
 
-	$this->resource('brands', 'BrandController');
 });
 
 $this->get('/', 'Site\SiteController@index');
