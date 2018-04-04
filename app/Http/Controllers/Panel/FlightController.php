e49beb5a94515febcec5f03fe1d22ce868f883c6
Bruno Roberto Gomes <bruno@unirg.edu.br>
2018-04-04 10:20:32 -0300

    6.15 - Validação dos vôos

--- app/Http/Controllers/Panel/FlightController.php
+++ app/Http/Controllers/Panel/FlightController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Flight;
 use App\Models\Plane;
 use App\Models\Airport;
+use App\Http\Requests\StoreUpdateFlightFormRequest;
 
 class FlightController extends Controller
 {
@@ -56,7 +57,7 @@ class FlightController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreUpdateFlightFormRequest $request)
     {
         $filename = null;
 
@@ -122,7 +123,7 @@ class FlightController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(StoreUpdateFlightFormRequest $request, $id)
     {
         $flight = $this->_flight->find($id);
 
