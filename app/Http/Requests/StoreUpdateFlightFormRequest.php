218ee17ce67ebcab897f6ed237498de489b950bc
Bruno Roberto Gomes <bruno@unirg.edu.br>
2018-04-04 10:23:05 -0300

    Corrigindo validação

--- app/Http/Requests/StoreUpdateFlightFormRequest.php
+++ app/Http/Requests/StoreUpdateFlightFormRequest.php
@@ -33,7 +33,7 @@ class StoreUpdateFlightFormRequest extends FormRequest
             'arrival_time'              => 'required',
             'old_price'                 => 'required',
             'price'                     => 'required',
-            'total_plots'               => 'required|digits_between:1,12',
+            'total_plots'               => 'required|integer|between:1,12',
             'price'                     => 'required',
             'is_promotion'              => 'boolean',
             'imagem'                    => 'image',
