a265c15b416db1f9034dd0314a1da32d9f28b0c3
Bruno Roberto Gomes <bruno@unirg.edu.br>
2018-04-06 16:45:58 -0300

    10.17 - Mostrar números dashboard

--- app/Http/Controllers/Panel/PanelController.php
+++ app/Http/Controllers/Panel/PanelController.php
@@ -4,13 +4,39 @@ namespace App\Http\Controllers\Panel;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Brand;
+use App\Models\Plane;
+use App\Models\State;
+use App\Models\City;
+use App\Models\Airport;
+use App\Models\Flight;
+use App\Models\Reserve;
+use App\User;
 
 class PanelController extends Controller
 {
 
 	public function index()
 	{
-		return view('panel.home.index');
+		$totalBrands 	= Brand::count();
+		$totalPlanes 	= Plane::count();
+		$totalStates 	= State::count();
+		$totalCities 	= City::count();
+		$totalAirports 	= Airport::count();
+		$totalFlights 	= Flight::count();
+		$totalUsers 	= User::count();
+		$totalReserves	= Reserve::count();
+
+		return view('panel.home.index', compact(
+			'totalBrands',
+			'totalPlanes',
+			'totalStates',
+			'totalCities',
+			'totalAirports',
+			'totalFlights',
+			'totalUsers',
+			'totalReserves'
+		));
 	}
 
 }
--- resources/views/panel/home/index.blade.php
+++ resources/views/panel/home/index.blade.php
@@ -12,22 +12,118 @@
 
 <div class="content-din">
 	
-	@for($i = 1; $i <= 10; $i++)
 	<div class="col-md-3 col-sm-4 col-xm-12">
 		<div class="rel-dash">
-			<i class="fa fa-home" aria-hidden="true"></i>
+			<i class="fa fa-university" aria-hidden="true"></i>
 			<div class="text-rel">
 				<h2 class="result">
-					{{$i}}
+					{{$totalBrands}}
 				</h2>
 				<h3 class="result-ds">
-					Total de Usuários
+					Total de marcas
 				</h3>
 			</div>
 		</div>
 	</div>
-	@endfor
 
-</div><!--Content Dinâmico-->
+	<div class="col-md-3 col-sm-4 col-xm-12">
+		<div class="rel-dash">
+			<i class="fa fa-plane" aria-hidden="true"></i>
+			<div class="text-rel">
+				<h2 class="result">
+					{{$totalPlanes}}
+				</h2>
+				<h3 class="result-ds">
+					Total de aviões
+				</h3>
+			</div>
+		</div>
+	</div>
+
+	<div class="col-md-3 col-sm-4 col-xm-12">
+		<div class="rel-dash">
+			<i class="fa fa-globe" aria-hidden="true"></i>
+			<div class="text-rel">
+				<h2 class="result">
+					{{$totalStates}}
+				</h2>
+				<h3 class="result-ds">
+					Total de estados
+				</h3>
+			</div>
+		</div>
+	</div>
+
+	<div class="col-md-3 col-sm-4 col-xm-12">
+		<div class="rel-dash">
+			<i class="fa fa-map-marker" aria-hidden="true"></i>
+			<div class="text-rel">
+				<h2 class="result">
+					{{$totalCities}}
+				</h2>
+				<h3 class="result-ds">
+					Total de cidades
+				</h3>
+			</div>
+		</div>
+	</div>
+
+	<div class="col-md-3 col-sm-4 col-xm-12">
+		<div class="rel-dash">
+			<i class="fa fa-thumb-tack" aria-hidden="true"></i>
+			<div class="text-rel">
+				<h2 class="result">
+					{{$totalAirports}}
+				</h2>
+				<h3 class="result-ds">
+					Total de aeroportos
+				</h3>
+			</div>
+		</div>
+	</div>
+
+	<div class="col-md-3 col-sm-4 col-xm-12">
+		<div class="rel-dash">
+			<i class="fa fa-fighter-jet" aria-hidden="true"></i>
+			<div class="text-rel">
+				<h2 class="result">
+					{{$totalFlights}}
+				</h2>
+				<h3 class="result-ds">
+					Total de vôos
+				</h3>
+			</div>
+		</div>
+	</div>
+
+	<div class="col-md-3 col-sm-4 col-xm-12">
+		<div class="rel-dash">
+			<i class="fa fa-users" aria-hidden="true"></i>
+			<div class="text-rel">
+				<h2 class="result">
+					{{$totalUsers}}
+				</h2>
+				<h3 class="result-ds">
+					Total de usuários
+				</h3>
+			</div>
+		</div>
+	</div>
+
+	<div class="col-md-3 col-sm-4 col-xm-12">
+		<div class="rel-dash">
+			<i class="fa fa-check-square" aria-hidden="true"></i>
+			<div class="text-rel">
+				<h2 class="result">
+					{{$totalReserves}}
+				</h2>
+				<h3 class="result-ds">
+					Total de reservas
+				</h3>
+			</div>
+		</div>
+	</div>
+
+</div>
 
 @endsection
